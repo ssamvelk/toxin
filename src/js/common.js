ebdc4e7fb36061f802beda9c83529400968c8258
Samvel <ssamvelk@yandex.ru>
2019-05-18 18:02:43 +0400

    Подготовил сборку webpack и фс  для проекта

---
+++ src/js/common.js
@@ -0,0 +1,1 @@
+console.log('PRIVET common.js тоже работает');
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+
+require('webpack-jquery-ui');
+require('webpack-jquery-ui/css');
+
+import './common.js';
+import '../scss/main.scss';
+
