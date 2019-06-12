e4bd870b6a5ad9665a4521738b9bd2a9020ab352
Samvel <ssamvelk@yandex.ru>
2019-06-13 01:58:31 +0400

    Переработан миксин comment и элемент like в целом. JS функция помещена в обработчик полной загрузки страницы, т.к. нажатие на сердечко не срабатывало

--- src/blocks/form_elements/like/like.js
+++ src/blocks/form_elements/like/like.js
@@ -1,12 +1,37 @@
-let likes=document.querySelectorAll('.like-wrp')
-for(let i=0; i<likes.length; i++){
-    likes[i].addEventListener('click', function(event){
-        let value = this.querySelector('.like-value').innerHTML
-        
-        if(this.classList.contains('heart--liked')) value--
-        else value++
 
-        this.querySelector('.like-value').innerHTML = value
-        this.classList.toggle('heart--liked')
-    })
-}
+
+document.addEventListener('DOMContentLoaded', function(){
+    let likes = document.querySelectorAll(".like-wrp")
+    
+    for(let i=0; i<likes.length; i++){
+        likes[i].addEventListener('click', function(event){
+            let value = this.querySelector('.like-value').innerHTML
+
+            if(this.classList.contains('heart--liked')) value--
+            else value++
+
+            this.querySelector('.like-value').innerHTML = value
+            this.classList.toggle('heart--liked')
+        })
+    }
+});
+
+
+/*
+$(function(){
+    let likes = document.querySelectorAll(".like-wrp")
+    
+    for(let i=0; i<likes.length; i++){
+        likes[i].addEventListener('click', function(event){
+            let value = this.querySelector('.like-value').innerHTML
+
+            if(this.classList.contains('heart--liked')) value--
+            else value++
+
+            this.querySelector('.like-value').innerHTML = value
+            this.classList.toggle('heart--liked')
+        })
+    }
+});
+
+*/
