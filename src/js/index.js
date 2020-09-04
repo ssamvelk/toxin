f731c7dbee202ccff9c14312e78449407db46c0a
Samvel <ssamvelk@yandex.ru>
2020-09-04 08:40:57 +0400

    выдернул из header/footer отдельные блоки navigation, navigation-list, logo #9 + рефакторинг проекта

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
 import '../scss/main.scss';
 
-import '../blocks/form-elements/dropdown/dropdown';
-import '../blocks/form-elements/dropdown/dropdown-buttons/dropdown-buttons';
-import '../blocks/form-elements/calendar/calendar';
-import '../blocks/form-elements/date-filter/date-filter';
-import '../blocks/form-elements/date-dropdown/date-dropdown';
-import '../blocks/form-elements/like/like';
-import '../blocks/form-elements/slider/slider';
-import '../blocks/form-elements/checkbox/checkbox-additional-list/checkbox-additional-list';
-import '../blocks/form-elements/carousel/carousel';
+import '../blocks/dropdown/dropdown';
+import '../blocks/dropdown/dropdown-buttons/dropdown-buttons';
+import '../blocks/calendar/calendar';
+import '../blocks/date-filter/date-filter';
+import '../blocks/date-dropdown/date-dropdown';
+import '../blocks/like/like';
+import '../blocks/slider/slider';
+import '../blocks/checkbox/checkbox-additional-list/checkbox-additional-list';
+import '../blocks/carousel/carousel';
 
 require('air-datepicker');
 require('webpack-jquery-ui/slider');
